8687ef098d9b18ceb1295420562d562ea6173b0e
Andre Madar <andre.madar1995@gmail.com>
2022-12-17 00:16:20 -0500

    Applying ptillemans fix
    https://github.com/ptillemans
    https://github.com/ThePrimeagen/harpoon/pull/238

--- lua/harpoon/ui.lua
+++ lua/harpoon/ui.lua
@@ -174,9 +174,10 @@ function M.nav_file(id)
 
     local mark = Marked.get_marked_file(idx)
     local filename = mark.filename
-    if filename:sub(1, 1) ~= "/" then
-        filename = vim.loop.cwd() .. "/" .. mark.filename
-    end
+--     if filename:sub(1, 1) ~= "/" then
+--         filename = vim.loop.cwd() .. "/" .. mark.filename
+--     end
+    local filename = vim.fs.normalize(mark.filename)
     local buf_id = get_or_create_buffer(filename)
     local set_row = not vim.api.nvim_buf_is_loaded(buf_id)
 
